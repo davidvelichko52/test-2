ae7034337346901f6b64c7d05ab290e0df38aa95
davidvelichko52 <davidvelichko52@gmail.com>
2020-05-01 15:44:37 -0700

    Done

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const methodOverride = require('method-override');
+let layouts = require('express-ejs-layouts')
+const db = require('./models')
 
 const app = express();
 
@@ -7,12 +9,52 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('static'));
 app.use(methodOverride('_method'));
+app.use(layouts)
 
 // WRITE YOUR ROUTES HERE /////////////////////
 
 app.get('/', (req, res) => {
-      res.render('home');
+    db.widget.findAll()
+      .then(widgets => {
+        
+        res.render('home', { widgets })
+        
+      })
+      .catch(err => {
+        console.log(err)
+        res.send('Error')
+      })
 })
+
+
+app.post('/', (req, res) => {
+    db.widget.create({
+        description: req.body.description,
+        quantity: req.body.quantity
+    })
+    .then(() => {
+        res.redirect('/')
+    })
+    .catch(err => {
+        console.log(err)
+        res.render('error')
+    })
+})
+
+
+app.delete('/', (req, res) => {
+    db.widget.destroy({
+        where: { id: req.body.id }
+    })
+    .then(() => {
+        res.redirect('/')
+    })
+    .catch(err => {
+        console.log('Error in delete route', err)
+        res.send('Error')
+    })
+})
+
 
 // Create a wildcard (catch-all)
 app.get('*', (req, res) => {
